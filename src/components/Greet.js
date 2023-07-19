e74b0f427bbfa3fecb178418f0d064a97d702e2f
ShwetaPal <shwetapal975@gmail.com>
2023-07-19 23:24:31 +0530

    test

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -2,14 +2,12 @@ import React, { useState } from "react";
 
 const Greet = () => {
   let [name, setName] = useState("");
-  const display = function (event) {
-    setName(event.target.value);
-  };
+  
   return (
     <div>
       <label htmlFor="name">Enter your name: </label>
-      <input type="text" onChange={display} value={name} />
-       {name.length > 0 && <p>Hello {name}!</p>}
+      <input type="text" onChange={(event)=> {setName("Hello " + event.target.value +"!")}} />
+       <p>{name}</p>
     </div>
   );
 };
